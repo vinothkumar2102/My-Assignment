5eedcba117e1f0e0346b783f80607cba4b448a98
vinothkumar2102 <vinothkumar.s@hotmail.com>
2026-07-08 22:06:18 +0530

    Add files via upload
    DAY1

---
+++ week1day2/JSdatatype.js
@@ -0,0 +1,27 @@
+
+var firstname ="Vinoth"
+ console.log(firstname);
+ console.log(typeof firstname);
+
+ var companyname ="infosys"
+ console.log(companyname);
+ console.log(typeof companyname);
+ 
+ var mobilenumaber ="9840361736"
+ console.log(mobilenumaber);
+ console.log(typeof mobilenumaber);
+
+ var isAutomation = true
+ console.log(isAutomation);
+ console.log(typeof isAutomation);
+ 
+ var hasplaywright
+ console.log(hasplaywright);
+ console.log(typeof hasplaywright);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
