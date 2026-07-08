3a0bde52ea07d62d87cafca1740606978a7053d8
vinothkumar2102 <vinothkumar.s@hotmail.com>
2026-07-08 22:09:52 +0530

    Add files via upload
    Day2

---
+++ week1day3/globalscoped.js
@@ -0,0 +1,14 @@
+let genderType = "female"
+function printgender() {
+    var color="brown"
+    if (genderType=="female") {
+        var age=30
+        let color ="pink"
+        console.log("color inside the block is",color);
+        
+    }
+    console.log("value of age is",age);
+    
+}
+printgender()
+console.log("value of genderType is",genderType);
